76c5942b64679444e87e0776b14975f64b84c000
Juan Manuel Vilardy Ortiz <ingvilardy121@gmail.com>
2025-11-18 02:59:58 +0000

    Cuarto programa del curso de Punteros en Codespaces

--- main.c
+++ main.c
@@ -1,28 +1,13 @@
 #include <stdio.h>
 
 void main() {
-    char *name_ptr = "PIYUSH";
-    char name_array[] = "piyush";
+    int i = 10;
 
-    // 1. Similarity between array and pointer
-    printf("name_ptr: %s, name_ptr: %p, *name_ptr: %c\n",
-            name_ptr,     name_ptr,     *name_ptr);
+    printf("i: %d, &i: %p, *&i: %d, *&*&i: %d, *&*&*&i: %d\n", i, &i, *&i, *&*&i, *&*&*&i);
 
-    printf("name_array: %s, name_array: %p, *name_array: %c\n",
-            name_array,     name_array,     *name_array);
+    *&*&*&i = 20;
+    printf("i: %d, &i: %p, *&i: %d, *&*&i: %d, *&*&*&i: %d\n", i, &i, *&i, *&*&i, *&*&*&i);
 
-    // 2. Difference between array and pointers #1
-    // printf("sizeof(name_ptr): %lu, sizeof(name_array): %lu\n", sizeof(name_ptr), sizeof(name_array));
-
-    int x[] = {10, 11, 12};
-    int *xptr = x;
-
-    // printf("sizeof(xptr): %lu, sizeof(x): %lu\n", sizeof(xptr), sizeof(x));
-
-    // 3. Array cannot be reassigned but pointer can be
-    int a[] = {10, 11, 12};
-    int i = 1024;
-//     a = &i;
-    xptr = a;
-    int *iptr = &i;
+//     &&&i = 30;
+//     printf("i: %d, &i: %p, *&i: %d, *&*&i: %d, *&*&*&i: %d\n", i, &i, *&i, *&*&i, *&*&*&i);
 }
