a26bd7835fb985bce939bf0b1332cef8be3abd79
Juan Manuel Vilardy Ortiz <ingvilardy121@gmail.com>
2025-11-19 04:51:35 +0000

    Doceavo programa del curso de punteros en Codespaces

--- main.c
+++ main.c
@@ -1,21 +1,38 @@
+// math_library.h
+#define ADD 0
+#define SUB 1
+#define MUL 2
+#define DIV 3
+
+void execute_operation(int operation, int a, int b);
+
+// math_library.c
+#include "math_library.h"
 #include <stdio.h>
 
-void function_1(int a) {
-  printf("function_1(): a = %d\n", a);
+static void add(int x, int y) { printf("add: %d\n", x + y); }
+
+static void sub(int x, int y) { printf("sub: %d\n", x - y); }
+
+static void mul(int x, int y) { printf("mul: %d\n", x * y); }
+
+static void div(int x, int y) { printf("div: %d\n", x / y); }
+
+typedef void (*func_t)(int a, int b);
+
+func_t func_lib[4] = {add, sub, mul, div};
+
+void execute_operation(int operation, int a, int b) {
+  func_lib[operation](a, b);
 }
 
-void function_2(int b) {
-  printf("function_2(): b = %d\n", b);
-}
+// main.c
+#include "math_library.h"
 
 int main() {
-    void (*pf)(int);
-    pf = function_1;
-    pf(10);
-
-    void (*qf)(int);
-    qf = &function_2;
-    qf(20);
-
-    return 0;
+  execute_operation(ADD, 100, 5);
+  execute_operation(SUB, 100, 5);
+  execute_operation(MUL, 100, 5);
+  execute_operation(DIV, 100, 5);
+  return 0;
 }
