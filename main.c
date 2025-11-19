91ff285ea775e47e1508b54d1fb3ea6aac03b8f7
Juan Manuel Vilardy Ortiz <ingvilardy121@gmail.com>
2025-11-19 18:21:00 +0000

    Doceavo v3 del curso de punteros en Codespaces

--- main.c
+++ main.c
@@ -1,33 +1,4 @@
-// math_library.h
-// void execute_operation(int operation, int a, int b);
-
-// math_library.c
-#include <stdio.h>
-#include "math.h"
-
-#define ADD 0
-#define SUB 1
-#define MUL 2
-#define DIV 3
-
-static void add(int x, int y) { printf("add: %d\n", x + y); }
-
-static void sub(int x, int y) { printf("sub: %d\n", x - y); }
-
-static void mul(int x, int y) { printf("mul: %d\n", x * y); }
-
-static void div(int x, int y) { printf("div: %d\n", x / y); }
-
-typedef void (*func_t)(int a, int b);
-
-func_t func_lib[4] = {add, sub, mul, div};
-
-void execute_operation(int operation, int a, int b) {
-  func_lib[operation](a, b);
-}
-
-// main.c
-// #include "math_library.h"
+#include "math_library.h"
 
 int main() {
   execute_operation(ADD, 100, 5);
