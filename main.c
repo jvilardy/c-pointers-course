56851a56e598b9933db318cd9c417fa13aa96e35
Juan Manuel Vilardy Ortiz <ingvilardy121@gmail.com>
2025-11-20 04:48:08 +0000

    Programa 17 de punteros en Codespaces

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define REQUEST 1 * 1024 * 1024 * 1024
+
 int main() {
-  int *p = (int *)malloc(400);
+  int *p;
 
-  printf("before: %d\n", *p);
-  *p = 4;
-  printf("after: %d\n", *p);
+  for (int i = 0; i < 1000000; i++) {
+    p = (int *)malloc(REQUEST);
+    printf("i:%d: p: %p\n", i, p);
+    if (p == NULL)
+      return -1;
+  }
 
-  free(p);
   return 0;
 }
