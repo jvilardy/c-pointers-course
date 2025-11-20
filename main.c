00539ecb25f7e24125a6aec3832d07ce423e617e
Juan Manuel Vilardy Ortiz <ingvilardy121@gmail.com>
2025-11-20 04:37:09 +0000

    Programa 15 de Punteros en Codespaces

--- main.c
+++ main.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  int i = 10;
-
-  void *pi = &i;
-
-  printf("pi: %p, &i: %p\n", pi, &i);
-  printf("*pi: %d\n", *(int *)pi);
-
-  return 0;
+  int *p = NULL;
+  printf("%p, 0x%llu\n", NULL, (long long unsigned int)NULL);
+  printf("%p\n", p);
 }
